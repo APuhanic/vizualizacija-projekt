e1b386dad9b5bcd9b5d90e81b0840a1d7d016dc0
APuhanic <puhanicantonio@gmail.com>
2023-06-05 16:33:49 +0200

    Refaktoriranje

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Define constants
+
 const SLIDER = document.getElementById("year-slider");
 const YEAR_LABEL = document.getElementById("year-label");
 const COUNTRY_LABEL = document.getElementById("country-label");
@@ -6,26 +6,25 @@ const SORT_BUTTON = document.querySelector(".data-button");
 const DROPDOWN_CONTENT = document.querySelector(".dropdown-content");
 const CURRENT_SORT = document.querySelector(".current-data");
 const backgroundColor = "rgba(8, 81, 156, 0.2)";
-// Define variables
+
 let currentData = "export";
 let selectedYear = 2022; // Set the default year
 let clickedCountry = "croatia";
 
-// Define SVG dimensions
+//  SVG dimenzije
 const width = window.innerWidth * 0.5;
 const height = window.innerHeight;
 
-// Define map projection
+//  map projection
 const projection = d3
   .geoMercator()
   .center([10, 55])
   .translate([width / 2, height / 2])
   .scale([width / 1.3]);
 
-// Define path generator
 const path = d3.geoPath().projection(projection);
 
-// Create SVG
+// kreiranje SVGa
 const svg = d3
   .select("#container")
   .append("svg")
@@ -33,7 +32,7 @@ const svg = d3
   .attr("height", height)
   .style("background-color", backgroundColor);
 
-//tooltip
+// tooltip
 const tooltip = svg
   .append("g")
   .attr("class", "tooltip")
@@ -53,15 +52,14 @@ tooltip
   .style("text-anchor", "middle")
   .style("font-size", "12px");
 
-// Create a group for zoomable elements
 const zoomGroup = svg.append("g");
 
-// Add event listeners
+// listeneri za interakcije
 SLIDER.addEventListener("input", handleSliderInput);
 SORT_BUTTON.addEventListener("click", toggleDropdown);
 DROPDOWN_CONTENT.addEventListener("click", handleDropdownClick);
 
-// Load GeoJSON data
+// GeoJSON data
 d3.json("europe_features.json").then(function (json) {
     countries = zoomGroup
     .selectAll("path")
@@ -75,15 +73,15 @@ d3.json("europe_features.json").then(function (json) {
     .on("mouseover", handleCountryMouseOver)
     .on("mouseout", handleCountryMouseOut);
 
-  // Enable zoom and panning
+  // zumiranje
   const zoom = d3.zoom().on("zoom", handleZoom);
   svg.call(zoom);
 
-  // Draw initial graph
+  // pocetno iscrtavanje grafa
   drawGraph();
 });
 
-// Event handlers 
+
 function handleSliderInput() {
   selectedYear = +this.value;
   YEAR_LABEL.textContent = selectedYear;
@@ -116,7 +114,6 @@ function handleCountryMouseOver(event, d) {
     .attr("transform", `translate(${event.pageX},${event.pageY - 20})`);
   tooltip.select("text").text(d.properties.name);
 
-  // Move the tooltip to the front
   tooltip.raise();
 }
 
@@ -130,57 +127,54 @@ function handleZoom(event) {
 }
 
 async function drawGraph() {
-  // Remove existing SVG for the bar chart
+  // brisanje starog bar charta
   d3.select("#container2").select("svg").remove();
 
-  const graphWidth = window.innerWidth * 0.5; // Adjust the width as desired
-  const graphHeight = window.innerHeight * 0.9; // Adjust the height as desired
-
-  // Create new SVG for the bar chart
+  const graphWidth = window.innerWidth * 0.5; 
+  const graphHeight = window.innerHeight * 0.9;
+  // kreiranje novog bar charta
   const svg2 = d3
     .select("#container2")
     .append("svg")
     .attr("width", graphWidth)
     .attr("height", graphHeight);
 
-  // Load the data from the corresponding JSON file
+  // učitavanje podataka
   const jsonFile = `/data/${currentData}/json/${clickedCountry}.json`;
   const data = await d3.json(jsonFile);
 
-  // Choose the year you want to visualize (e.g., 2022)
+  // izabiranje godine
   const year =
     currentData === "import"
       ? `Imported value in ${selectedYear}`
       : `Exported value in ${selectedYear}`;
 
-  // Filter the data for the selected year
+  // filtiriranje podataka za izabranu godinu
   const filteredData = data.filter(
     (d) => d.Code !== "TOTAL" && d[year] !== undefined
   );
 
-  // Sort the filtered data by value in descending order
+  // soriiranje podataka
   filteredData.sort((a, b) => b[year] - a[year]);
 
-  // Extract the top 10 entries from the filtered data
+  // izdvajanje prvih 10 podataka
   const topData = filteredData.slice(0, 10);
 
-  // Create scales for the x and y axes
+
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(topData, (d) => d[year])])
-    .range([0, graphWidth - 300]); // Adjust the range to fit within the SVG
-
+    .range([0, graphWidth - 300]); 
   const yScale = d3
     .scaleBand()
     .domain(topData.map((d) => d["Product label"]))
-    .range([50, graphHeight - 200]) // Adjust the range to fit within the SVG
+    .range([50, graphHeight - 200]) 
     .padding(0.1);
 
-  // Create x and y axes
   const xAxis = d3.axisBottom(xScale).tickFormat((d) => d);
   const yAxis = d3.axisLeft(yScale);
 
-  // Append x axis to the SVG
+  // dodavanje x osi na svg
   svg2
     .append("g")
     .attr("class", "x axis")
@@ -193,7 +187,7 @@ async function drawGraph() {
     .attr("dy", ".15em")
     .attr("transform", "rotate(-45)");
 
-  // Append y axis to the SVG
+  // dodavanje y osi na svg
   svg2
     .append("g")
     .attr("class", "y axis")
@@ -201,12 +195,11 @@ async function drawGraph() {
     .call(yAxis)
     .selectAll("text")
     .style("font-size", "14px");
-  // Add grid lines for each value on the x-axis
+  // linije
   svg2
     .append("g")
     .attr("class", "grid")
     .attr("transform", `translate(150, ${graphHeight - 200})`)
-    //add opacity to lines
     .attr("opacity", 0.1)
     .call(
       d3
@@ -214,7 +207,7 @@ async function drawGraph() {
         .tickSize(-graphHeight + 250)
         .tickFormat("")
     );
-  //add a title for x axis
+  // naslov za x os
   svg2
     .append("text")
     .attr("class", "x axis")
@@ -224,7 +217,7 @@ async function drawGraph() {
     .style("font-weight", "bold")
     .text("Value in $ thousands");
 
-  //add a title for y axis above the axis
+  //naslov za y os
   svg2
     .append("text")
     .attr("class", "y axis")
@@ -234,7 +227,7 @@ async function drawGraph() {
     .style("font-weight", "bold")
     .text("Products");
 
-  // Create horizontal bars
+  // bar chart 
   const bars = svg2
     .selectAll(".bar")
     .data(topData)
@@ -243,14 +236,14 @@ async function drawGraph() {
     .attr("class", "bar")
     .attr("x", 151)
     .attr("y", (d) => {
-      const barHeight = yScale.bandwidth() * 0.8; // Adjust the factor as desired
+      const barHeight = yScale.bandwidth() * 0.8; 
       return yScale(d["Product label"]) + (yScale.bandwidth() - barHeight) / 2;
     })
     .attr("width", (d) => xScale(d[year]))
-    .attr("height", yScale.bandwidth() * 0.8) // Adjust the factor as desired
+    .attr("height", yScale.bandwidth() * 0.8) 
     .attr("fill", "steelblue");
 
-  // Add labels to the bars
+  // labela za bar chart
   const labels = svg2
     .selectAll(".label")
     .data(topData)
